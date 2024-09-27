d98113f0202c0b1ee2a31397b7ae796f0876534e
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-09-27 01:59:35 -0300

    base64-last commit

--- Message-encoder/src/main/java/Groups/Message_encoder/controller/EncoderController.java
+++ Message-encoder/src/main/java/Groups/Message_encoder/controller/EncoderController.java
@@ -1,10 +1,29 @@
 package Groups.Message_encoder.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import Groups.Message_encoder.service.EncoderService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/translate")
 public class EncoderController {
-
+	private final EncoderService service;
+	
+	@PostMapping("/encodeBase64")
+	public ResponseEntity<String> encodeBase64(@RequestBody String text) {
+	String codifica = service.toBase64(text);
+	return ResponseEntity.ok(codifica);
+	}
+	
+	@PostMapping("/decodeBase64")
+	public ResponseEntity<String> decodeBase64(@RequestBody String text){
+		String decodifica = service.fromBase64(text);
+		return ResponseEntity.ok(decodifica);
+	}
 }
--- Message-encoder/src/main/java/Groups/Message_encoder/service/EncoderService.java
+++ Message-encoder/src/main/java/Groups/Message_encoder/service/EncoderService.java
@@ -1,8 +1,28 @@
 package Groups.Message_encoder.service;
 
+import java.util.Base64;
+
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class EncoderService {
 
+	public String toBase64(String txt) {
+		Base64.Encoder encoder = Base64.getEncoder();
+		String encodeText = encoder.encodeToString(txt.getBytes());
+		 
+		return new String(encodeText);
+	}
+	
+	public String fromBase64(String txt) {
+		if (txt != null) {
+			byte[] decodedBytes = Base64.getDecoder().decode(txt);
+	        return new String(decodedBytes);
+		}else {
+			throw new IllegalArgumentException("Erro ao decodificar a string Base64");
+		}
+		}
 }
